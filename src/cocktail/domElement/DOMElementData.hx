1dd66cb0a735f0b7a8577e64fe916f7a82def042
yanhick <yannick.dominguez@gmail.com>
2011-09-23 16:09:47 +0200

    starting to implement styles in cocktail

--- src/cocktail/domElement/DOMElementData.hx
+++ src/cocktail/domElement/DOMElementData.hx
@@ -178,3 +178,287 @@ package cocktail.domElement;
 		round;
 		bevel;
 	}
+	
+//////////////////////////////////////////////////////////////////////////////////////////
+// Styled DOMElement structures and enums
+// lists all the supported styles for DOMElements
+//////////////////////////////////////////////////////////////////////////////////////////	
+
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// Structures
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * List all the available styles
+	 */
+	typedef StyleData = {
+		
+		/**
+		 * margins
+		 */
+		var marginLeft:MarginStyleValue;
+		var marginRight:MarginStyleValue;
+		var marginTop:MarginStyleValue;
+		var marginBottom:MarginStyleValue;
+		
+		/**
+		 * paddings
+		 */
+		var paddingLeft:PaddingStyleValue;
+		var paddingRight:PaddingStyleValue;
+		var paddingTop:PaddingStyleValue;
+		var paddingBottom:PaddingStyleValue;
+		
+		/**
+		 * The way a DOMElement flows in a document
+		 */
+		var display:DisplayStyleValue;
+		
+		/**
+		 * The way a DOMElement position himself
+		 * relative to a parent
+		 */
+		var position:PositionStyleValue;
+		
+		/**
+		 * dimensions of a DOMElement
+		 */
+		var width:DimensionStyleValue;
+		var height:DimensionStyleValue;
+		
+		/**
+		 * dimensions constraint of a DOMElement
+		 */
+		var minHeight:ConstrainedDimensionStyleValue;
+		var maxHeight:ConstrainedDimensionStyleValue;
+		var minWidth:ConstrainedDimensionStyleValue;
+		var maxWidth:ConstrainedDimensionStyleValue;
+		
+		/**
+		 * offsets of a DOMElement
+		 */
+		var top:PositionOffsetStyleValue;
+		var left:PositionOffsetStyleValue;
+		var bottom:PositionOffsetStyleValue;
+		var right:PositionOffsetStyleValue;
+	}
+
+	/**
+	 * Represents the width and height of a DOMElement's
+	 * parent content, into which it can be placed
+	 */
+	typedef ContainingDOMElementDimensions = {
+		var width:Int;
+		var height:Int;
+	}
+	
+	/**
+	 * Represents the total width and height of a 
+	 * DOMElement, including its content, margins and paddings
+	 */
+	typedef OffsetDOMElementDimensions = {
+		var width:Int;
+		var height:Int;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// Enums
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Dimensions values for margin thickness
+	 */
+	enum MarginStyleValue {
+		
+		/**
+		 * absolute thickness
+		 */
+		length(value:LengthValue);
+		
+		/**
+		 * thickness relatvie to the
+		 * parent DOMElement's widthor height
+		 */
+		percent(value:Int);
+		
+		/**
+		 * Margin takes the remaining space
+		 * in the parent DOMElement width or height
+		 */
+		auto;
+	}
+	
+	/**
+	 * Dimensions value for padding thickness
+	 */
+	enum PaddingStyleValue {
+		
+		/**
+		 * absolute thickness
+		 */
+		length(value:LengthValue);
+		
+		/**
+		 * thickness relative to the
+		 * parent DOMElement width or height
+		 */
+		percent(value:Int);
+	}
+	
+	/**
+	 * Display of a DOMElement in a 
+	 * DOMElements flow
+	 */
+	enum DisplayStyleValue {
+		
+		/**
+		 * add a line break before
+		 * and after the DOMElement. Act
+		 * as a box with margins, paddings
+		 * and dimensions
+		 */
+		block;
+		
+		/**
+		 * Displays as block but is placed
+		 * on the current line in the flow
+		 */
+		inlineBlock;
+		
+		/**
+		 * Displayed on the current line. 
+		 * Doesn't have block's attributes
+		 * such as margins, paddings...
+		 */
+		_inline;
+		
+		/**
+		 * The DOMElement isn't displays
+		 * and doesn't influence the flow
+		 */
+		none;
+	}
+	
+	/**
+	 * Determine how a DOMElement is positioned relative
+	 * to its ancestors
+	 */
+	enum PositionStyleValue {
+		
+		/**
+		 * Appear after its parent in the flow, on a
+		 * new line or the current one based on its
+		 * display
+		 */
+		_static;
+		
+		/**
+		 * same as static but an offset can be applied
+		 * with the top, left, right and bottom styles
+		 * once the DOMElement is placed in the flow
+		 */
+		relative;
+		
+		/**
+		 * The DOMElement is outside of the flow, it 
+		 * is placed using its left, right, bottom and
+		 * top offsets, its origin being its first ancestor
+		 * with a position different from static
+		 */
+		absolute;
+		
+		/**
+		 * The DOMElement is outside the flow, it
+		 * is placed using its left, right, bottom
+		 * and top offsets, its origin being the first
+		 * DOMElement (the one at the top of the 
+		 * hierarchy)
+		 */
+		fixed;
+	}
+	
+	/**
+	 * The offset applied to a DOMElement
+	 * once positioned
+	 */
+	enum PositionOffsetStyleValue {
+		
+		/**
+		 * absolute offset
+		 */
+		length(value:LengthValue);
+		
+		/**
+		 * relative to the parent
+		 * DOMElement width or heigt
+		 */
+		percent(value:Int);
+		
+		/**
+		 * no offset
+		 */
+		auto;
+	}
+	
+	/**
+	 * The dimension of the content of 
+	 * a DOMElement (without margins 
+	 * and paddings)
+	 */
+	enum DimensionStyleValue {
+		
+		/**
+		 * absolute value
+		 */
+		length(value:LengthValue);
+		
+		/**
+		 * relative the parent DOMElement
+		 * dimensions
+		 */
+		percent(value:Int);
+		
+		/**
+		 * takes the remaining space in
+		 * the parent element width or 
+		 * height
+		 */
+		auto;
+	}
+	
+	/**
+	 * The constraint applied to the content
+	 * of a DOMElement contrained its max/min
+	 * width or height
+	 */
+	enum ConstrainedDimensionStyleValue {
+		
+		/**
+		 * absolute value
+		 */
+		length(value:LengthValue);
+		
+		/**
+		 * relative to the parent DOMElement
+		 * dimensions 
+		 */
+		percent(value:Int);
+		
+		/**
+		 * No constraint is enforced
+		 */
+		none;
+	}
+	
+	/**
+	 * Lists the different types of 
+	 * unit supported with an explicitly
+	 * set length such as px, cm, em...
+	 */
+	enum LengthValue {
+		
+		/**
+		 * a length defined in screen pixels
+		 */
+		pixel(value:Int);
+	}
