6fa7dc600454499acdf91437e93612bbe12da30b
lexoyo <a.hoyau@silexlabs.org>
2011-12-05 18:32:59 +0100

    fonts in progress

--- src/cocktail/resource/ResourceLoaderManager.hx
+++ src/cocktail/resource/ResourceLoaderManager.hx
@@ -1,7 +1,5 @@
 /*
-This file is part of Silex - see http://projects.silexlabs.org/?/silex
-
-Silex is © 2010-2011 Silex Labs and is released under the GPL License:
+This project is © 2010-2011 Silex Labs and is released under the GPL License:
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
 
