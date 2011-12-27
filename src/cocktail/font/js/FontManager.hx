989c7ab4c935c7c0032b492ad8f54e4e6116466e
lexoyo <a.hoyau@silexlabs.org>
2011-12-27 15:38:04 +0100

    fonts merged

--- src/cocktail/font/js/FontManager.hx
+++ src/cocktail/font/js/FontManager.hx
@@ -1,9 +1,13 @@
 /*
-	This file is part of Cocktail http://www.silexlabs.org/groups/labs/cocktail/
-	This project is © 2010-2011 Silex Labs and is released under the GPL License:
-	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
-	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-	To read the license please visit http://www.gnu.org/copyleft/gpl.html
+This file is part of Cocktail http://www.silexlabs.org/groups/labs/cocktail/
+
+This project is © 2010-2011 Silex Labs and is released under the GPL License:
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+To read the license please visit http://www.gnu.org/copyleft/gpl.html
 */
 package cocktail.font.js;
 
@@ -44,7 +48,6 @@ class FontManager extends AbstractFontManager
 	 * This is the name you want to put in the css class to apply the style to some text.
 	 * @param	successCallback the callback which must be called once the file is successfully done loading
 	 * @param	errorCallback the callback which must be called if there was an error during loading
-	 * @param	allowCache wheter to allow the browser to cache the loaded file
 	 */
 	public static function loadFont(url:String, name:String, successCallback:Void->Void, errorCallback:String->Void):Void
 	{
@@ -74,7 +77,9 @@ class FontManager extends AbstractFontManager
 		Lib.document.getElementsByTagName("head")[0].appendChild(styleE);
 		
 		// to do: detect css loading errors
+		// ?? styleE.async = 'true'; styleE.onload = tk.onreadystatechange = 
 		successCallback();
+		
 		if (fonts == null) fonts = new Array();
 		fonts.push(fontData);
 	}
