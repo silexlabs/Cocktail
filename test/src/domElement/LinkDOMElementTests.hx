8f683f9b2ce54f391ab5b82922decf0f03f09343
yanhick <yannick.dominguez@gmail.com>
2012-01-23 14:59:58 +0100

    added unit tests for LinkDOMElement

---
+++ test/src/domElement/LinkDOMElementTests.hx
@@ -0,0 +1,72 @@
+/*
+This file is part of Silex - see http://projects.silexlabs.org/?/silex
+
+Silex is © 2010-2011 Silex Labs and is released under the GPL License:
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+To read the license please visit http://www.gnu.org/copyleft/gpl.html
+*/
+
+package domElement;
+
+import cocktail.domElement.BodyDOMElement;
+import cocktail.domElement.LinkDOMElement;
+import cocktail.domElement.DOMElementData;
+import cocktail.textElement.TextElement;
+import cocktail.style.StyleData;
+import utest.Assert;
+import utest.Runner;
+import utest.ui.Report;
+import utest.ui.common.HeaderDisplayMode;
+
+/**
+ * Units tests for DOMElements
+ * @author Yannick DOMINGUEZ & Raphael HARMEL
+ * 
+ * 2011-08-10 PHP part is not complete yet.
+ * 
+ */
+
+class LinkDOMElementTests 
+{
+	
+	public static function main()
+	{	
+		var runner = new Runner();
+		runner.addCase(new LinkDOMElementTests());
+		Report.create(runner);
+		runner.run();
+	}
+	
+	public function new() 
+	{
+	}
+	
+	public function testLinkDOMElement()
+	{
+		var bodyDOMElement:BodyDOMElement = new BodyDOMElement();
+		
+		var linkDOMElement1:LinkDOMElement = new LinkDOMElement();
+		linkDOMElement1.style.display = DisplayStyleValue.block;
+		linkDOMElement1.addText(new TextElement("self"));
+		
+		var linkDOMElement2:LinkDOMElement = new LinkDOMElement();
+		linkDOMElement2.style.display = DisplayStyleValue.block;
+		linkDOMElement2.addText(new TextElement("blank"));
+		
+		bodyDOMElement.addChild(linkDOMElement1);
+		bodyDOMElement.addChild(linkDOMElement2);
+		
+		linkDOMElement1.href = "bim";
+		linkDOMElement1.target = LinkTargetValue.self;
+		
+		linkDOMElement2.href = "bam";
+		linkDOMElement2.target = LinkTargetValue.blank;
+		
+		linkDOMElement2.href = "zing";
+
+	}
+}
