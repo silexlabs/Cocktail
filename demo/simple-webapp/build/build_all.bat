7bd5d185fa2c2fb80045d8cad87dcbaf7772c418
Raphael Harmel <raphael.harmel@gmail.com>
2012-02-14 17:23:23 +0100

    simple-webapp demo build files update

--- demo/simple-webapp/build/build_all.bat
+++ demo/simple-webapp/build/build_all.bat
@@ -5,7 +5,7 @@ ECHO starting full build process...
 WebApp_as3.hxml
 WebApp_js.hxml
 cd native_apps
-CALL WebApp_PhoneGapBuild.bat
+CALL WebApp_PhoneGapBuild_archive.bat
 CALL WebApp_air_desktop_and_tv.bat
 CALL WebApp_air_android.bat
 CALL WebApp_air_iOS.bat
