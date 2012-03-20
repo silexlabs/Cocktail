f521a26895a1bb9d6f02342cd5708475437834db
yanhick <yannick.dominguez@gmail.com>
2012-03-20 11:49:57 +0100

    refactoring bodyDOMElement

---
+++ src/core/dom/HTMLDocument.hx
@@ -0,0 +1,24 @@
+package core.dom;
+
+/**
+ * ...
+ * @author Yannick DOMINGUEZ
+ */
+
+class HTMLDocument extends Document
+{
+
+	private var _body:HTMLBodyElement;
+	public var body(get_body, never):HTMLBodyElement;
+	
+	public function new() 
+	{
+		super();
+		_body = new HTMLBodyElement();
+	}
+	
+	private function get_body():HTMLBodyElement 
+	{
+		return _body;
+	}
+}
