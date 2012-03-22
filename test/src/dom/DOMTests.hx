64858dcd799d2711f5f0d27961b2e145c02dddd9
yanhick <yannick.dominguez@gmail.com>
2012-03-22 15:28:56 +0100

    add test files

--- test/src/dom/DOMTests.hx
+++ test/src/dom/DOMTests.hx
@@ -13,38 +13,62 @@ To read the license please visit http://www.gnu.org/copyleft/gpl.html
 package dom;
 
 
-import utest.Assert;
-import utest.Runner;
-import utest.ui.Report;
-import utest.ui.common.HeaderDisplayMode;
-
-import core.dom.HTMLElement;
-import core.dom.HTMLBodyElement;
-import core.dom.HTMLImageElement;
-import core.dom.HTMLAnchorElement;
-import core.dom.HTMLCanvasElement;
-import core.dom.HTMLDocument;
+import haxe.Log;
+import core.HTMLElement;
+import core.html.HTMLBodyElement;
+import core.html.HTMLImageElement;
+import core.HTMLAnchorElement;
+import core.html.HTMLCanvasElement;
+import core.html.HTMLDocument;
 import core.Text;
+import core.HTMLInputElement;
+import core.style.StyleData;
+import core.unit.UnitData;
 
 class DOMTests 
 {
 	
 	public static function main()
 	{	
-		var runner = new Runner();
+		/**var runner = new Runner();
 		runner.addCase(new DOMTests());
 		Report.create(runner);
-		runner.run();
+		runner.run();*/
+		new DOMTests();
 	}
 	
 	public function new() 
 	{
-		
+		testDOM();
 	}
 	
 	public function testDOM()
 	{
+		var document = new HTMLDocument();
+		var img = new HTMLImageElement();
+		img.src = "testPicture.jpg";
+		//img.style.floatStyle = FloatStyle.left;
 		
+		var container = new HTMLElement();
+		container.appendChild(img);
+		container.style.display = Display.block;
+		
+		var text = new core.Text();
+		text.nodeValue = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit nisl ac erat blandit eget dictum erat feugiat. Praesent vitae dolor vitae tellus luctus vulputate non eu neque. Sed vitae tortor dolor. Etiam tempus iaculis pretium. Sed ac est lorem, vel commodo diam. Vestibulum ultricies nisl in risus ultricies facilisis. Ut accumsan ullamcorper elementum. Maecenas tincidunt odio ut risus egestas convallis. Etiam ornare, orci ut viverra vehicula, purus nunc fermentum tellus, at gravida urna purus et tellus. Sed urna nulla, rhoncus a ultricies eget, facilisis vitae lorem. Duis eget nisl nunc. Nullam sit amet enim ac libero suscipit tempor id sed mauris. Pellentesque fermentum elementum nisl quis convallis. Donec vulputate aliquam congue. Vivamus sed arcu eu massa pharetra commodo in vitae purus. Sed nunc leo, consectetur ac fringilla in, mattis semper arcu.";
+		
+		container.appendChild(text);
+		
+		container.style.width = Dimension.percent(50);
+		container.style.marginLeft = Margin.autoValue;
+		container.style.marginRight = Margin.autoValue;
+		
+		document.body.style.backgroundImage = [BackgroundImage.image(ImageValue.url("testPicture.jpg"))];
+		
+		
+		
+		container.style.backgroundColor = BackgroundColor.colorValue(ColorValue.keyword(ColorKeyword.fuchsia));
+		
+		document.body.appendChild(container);
 		
 	}
 }
