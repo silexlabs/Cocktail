0862f7cbf23ae667aa82c8366965eef20cdd2c2a
yanhick <yannick.dominguez@gmail.com>
2012-03-28 10:32:21 +0200

    added doc

--- src/core/window/AbstractWindow.hx
+++ src/core/window/AbstractWindow.hx
@@ -1,32 +1,64 @@
+/*
+	This file is part of Cocktail http://www.silexlabs.org/groups/labs/cocktail/
+	This project is © 2010-2011 Silex Labs and is released under the GPL License:
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	To read the license please visit http://www.gnu.org/copyleft/gpl.html
+*/
 package core.window;
+
+import core.dom.Document;
 import core.event.Event;
 import core.event.IEventTarget;
-import core.html.HTMLDocument;
 
 /**
- * TODO : doc
+ * Represents the window through which the Document is
+ * viewed
  * 
  * @author Yannick DOMINGUEZ
  */
 class AbstractWindow implements IEventTarget
 {
-
+	/**
+	 * callback called when the window is resized, such as
+	 * when in a browser, the browser is resized
+	 */
 	private var _onResize:Event->Void;
 	public var onResize(get_onResize, set_onResize):Event->Void;
 	
-	private var _document:HTMLDocument;
-	public var document(get_document, never):HTMLDocument;
+	/**
+	 * return the document viewed through the window
+	 */
+	private var _document:Document;
+	public var document(get_document, never):Document;
 	
+	/**
+	 * Height (in pixels) of the browser window viewport including,
+	 * if rendered, the horizontal scrollbar.
+	 */
 	public var innerHeight(get_innerHeight, never):Int;
 	
+	/**
+	 * Width (in pixels) of the browser window viewport including,
+	 * if rendered, the vertical scrollbar.
+	 */
 	public var innerWidth(get_innerWidth, never):Int;
 	
-	//TODO : Window should instantiate Document but window object
-	//is instantiated multiple time to listen to resize event
-	public function new(document:HTMLDocument = null) 
+	/**
+	 * class constructor. Store a ref
+	 * to the provided document
+	 * 
+	 * TODO : Window should instantiate Document but window object
+	 * is instantiated multiple time to listen to resize event
+	 */
+	public function new(document:Document = null) 
 	{
 		_document = document;
 	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// SETTERS/GETTERS
+	//////////////////////////////////////////////////////////////////////////////////////////
 	
 	private function set_onResize(value:Event->Void):Event->Void
 	{
@@ -48,9 +80,8 @@ class AbstractWindow implements IEventTarget
 		return -1;
 	}
 	
-	private function get_document():HTMLDocument
+	private function get_document():Document
 	{
 		return _document;
 	}
-	
 }
