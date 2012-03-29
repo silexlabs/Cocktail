945572c68243980163eaf21f7ef315e9aa82d02c
Raphael Harmel <raphael.harmel@gmail.com>
2012-03-29 19:26:47 +0200

    market demo: added phonegap build & binary files

---
+++ demo/market/build/build_all.bat
@@ -0,0 +1,20 @@
+ECHO OFF
+ECHO.
+ECHO starting full build process...
+
+REM WebApp_as3.hxml
+WebApp_js.hxml
+cd native_apps
+CALL WebApp_PhoneGapBuild_archive.bat
+REM CALL WebApp_air_desktop_and_tv.bat
+REM CALL WebApp_air_android.bat
+REM CALL WebApp_air_iOS.bat
+cd ..
+
+ECHO.
+ECHO full build process complete
+ECHO.
+
+ECHO ON
+
+PAUSE
---
+++ demo/market/build/native_apps/WebApp_PhoneGapBuild_archive_Dev.bat
@@ -0,0 +1,41 @@
+ECHO OFF
+ECHO.
+SET config_file=config.xml
+SET input_files_path=icons splashScreens_PhoneGap config.xml index.html ../../bin/default.css ../../bin/assets ../../bin/WebApp.js
+SET output_file_path=WebApp_PhoneGapBuild_Dev.zip
+
+cls
+
+ECHO copying splashScreens directory to splashScreens_PhoneGap...
+xcopy splashScreens splashScreens_PhoneGap /Q /I
+ECHO.
+
+ECHO preparing %config_file% file...
+copy config_Dev.xml %config_file%
+ECHO.
+
+ECHO copying index file...
+copy ..\..\bin\WebApp_js.html index.html
+ECHO.
+
+ECHO renaming files creating issues...
+move splashScreens_PhoneGap\Default@2x.png splashScreens_PhoneGap\Default2x.png
+ECHO.
+
+ECHO Removing %output_file_path%...
+del %output_file_path% /Q /S
+
+ECHO.
+
+ECHO Compressing %output_file_path%...
+C:\"Program Files"\7-Zip\7z.exe a -tzip %output_file_path% %input_files_path%
+ECHO.
+
+ECHO Removing %config_file%, index.html and /splashScreens_PhoneGap...
+del %config_file% /Q /S
+del index.html /Q /S
+rd splashScreens_PhoneGap /Q /S
+ECHO.
+
+ECHO Compression complete
+ECHO.
