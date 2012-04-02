4d8cffdb657dfdfdfd7e98ae224f3dce3030a178
yanhick <yannick.dominguez@gmail.com>
2012-04-02 15:00:03 +0200

    adding html unit tests

---
+++ test/src/html/HTMLTests.hx
@@ -0,0 +1,52 @@
+/*
+This file is part of Silex - see http://projects.silexlabs.org/?/silex
+
+Silex is © 2010-2011 Silex Labs and is released under the GPL License:
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+To read the license please visit http://www.gnu.org/copyleft/gpl.html
+*/
+
+package html;
+
+import cocktail.Cocktail;
+import cocktail.Lib;
+import core.dom.Attr;
+import core.dom.Element;
+import core.dom.NamedNodeMap;
+import core.dom.Node;
+import core.dom.Text;
+
+import utest.Assert;
+import utest.Runner;
+import utest.ui.Report;
+
+class HTMLTests 
+{
+	
+	public static function main()
+	{	
+		var runner = new Runner();
+		runner.addCase(new HTMLTests());
+		Report.create(runner);
+		runner.run();
+	}
+	
+	public function new() 
+	{
+		
+	}
+	
+	function testHTMLElement()
+	{
+		var htmlEl = new HTMLElement("div");
+		Lib.document.body.appendChild(htmlEl);
+		
+		htmlEl.style.width = Dimension.length(px(50));
+		
+		Assert.equals(htmlEl.clientWidth, 50);
+	}
+}
