6d7334a40be033b6c88e82098d40d158e64dc9f2
yanhick <yannick.dominguez@gmail.com>
2012-04-05 19:24:31 +0200

    removed embedded core style which didn t added anything

--- src/cocktail/core/html/AbstractHTMLInputElement.hx
+++ src/cocktail/core/html/AbstractHTMLInputElement.hx
@@ -7,7 +7,7 @@
 */
 package cocktail.core.html;
 
-import cocktail.core.style.EmbeddedCoreStyle;
+import cocktail.core.style.CoreStyle;
 
 #if (flash9 || nme)
 import cocktail.port.flash_player.HTMLElement;
@@ -99,7 +99,7 @@ class AbstractHTMLInputElement extends HTMLElement
 	 */
 	override private function initCoreStyle():Void
 	{
-		_coreStyle = new EmbeddedCoreStyle(this);
+		_coreStyle = new CoreStyle(this);
 	}
 	
 	
