a05a83f0022afbc26b271f70dcdb8b00768e008d
yanhick <yannick.dominguez@gmail.com>
2012-05-16 16:09:47 +0200

    working on scrollbar

--- src/cocktail/core/html/ScrollBar.hx
+++ src/cocktail/core/html/ScrollBar.hx
@@ -21,49 +21,101 @@ class ScrollBar extends HTMLElement
 	
 	private var _scrollThumb:HTMLElement;
 	
+	private var _upArrow:HTMLElement;
+	
+	private var _downArrow:HTMLElement;
+	
 	public function new(isVertical:Bool) 
 	{
 		
 		_isVertical = isVertical;
+		
 		super("");
 		
-		
-		_style.position = "absolute";
-		
+		_scrollThumb = new HTMLElement("");
+		_upArrow = new HTMLElement("");
+		_downArrow = new HTMLElement("");
 	
+	
+		
+		initScrollBar();
 		
 		if (_isVertical)
 		{
-			_style.backgroundColor = "rgba(255,0,0,0.7)";
-			_style.height = "100%";
-			_style.width = "15px";
-			_style.right = "0";
-			_style.top = "0";
+			initVerticalScrollBar();
 		}
 		else
 		{
-			_style.backgroundColor = "rgba(0,0,255,0.7)";
-			_style.width = "100%";
-			_style.height = "15px";
-			_style.bottom = "0";
-			_style.left = "0";
+			initHorizontalScrollBar();
 		}
-		
-		_style.display = "block";
-		
-		
-		_scrollThumb = new HTMLElement("");
-		appendChild(_scrollThumb);
 		
 		//_scrollThumb.onmousedown = onThumbMouseDown;
 		
-		_scrollThumb.style.backgroundColor = "green";
-		_scrollThumb.style.width = "15px";
-		_scrollThumb.style.height = "15px";
-		_scrollThumb.style.position = "absolute";
-		_scrollThumb.style.display = "block";
+		appendChild(_scrollThumb);
+		appendChild(_upArrow);
+		appendChild(_downArrow);
 		
 		onmousedown = onTrackMouseDown;
+		
+		_downArrow.onmousedown = onDownArrowMouseDown;
+		_upArrow.onmousedown = onUpArrowMouseDown;
+		
+	}
+	
+	private function initScrollBar():Void
+	{
+		_style.backgroundColor = "red";
+		_style.display = "block";
+		_style.position = "absolute";
+		
+		_scrollThumb.style.backgroundColor = "green";
+		_scrollThumb.style.position = "absolute";
+		_scrollThumb.style.display = "block";
+		_scrollThumb.style.width = "15px";
+		_scrollThumb.style.height = "15px";
+		
+		_upArrow.style.backgroundColor = "orange";
+		_upArrow.style.position = "absolute";
+		_upArrow.style.display = "block";
+		_upArrow.style.width = "15px";
+		_upArrow.style.height = "15px";
+		
+		_downArrow.style.backgroundColor = "orange";
+		_downArrow.style.position = "absolute";
+		_downArrow.style.display = "block";
+		_downArrow.style.width = "15px";
+		_downArrow.style.height = "15px";
+		
+	}
+	
+	private function initVerticalScrollBar():Void
+	{
+		_style.height = "100%";
+		_style.width = "15px";
+		_style.right = "0";
+		_style.top = "0";
+		
+		_downArrow.style.bottom = "0";
+	}
+	
+	private function initHorizontalScrollBar():Void
+	{
+		_style.width = "100%";
+		_style.height = "15px";
+		_style.bottom = "0";
+		_style.left = "0";
+		
+		_downArrow.style.right = "0";
+	}
+	
+	private function onDownArrowMouseDown(event:MouseEvent):Void
+	{
+		scroll += 50;
+	}
+	
+	private function onUpArrowMouseDown(event:MouseEvent):Void
+	{
+		scroll -= 50;
 	}
 	
 	//TODO : should add event listener to body instead of callback
@@ -84,27 +136,11 @@ class ScrollBar extends HTMLElement
 	{
 		if (_isVertical == true)
 		{
-		//	_scrollThumb.style.top = event.clientY + "px";
+			scroll = event.clientY;
 		}
 		else
 		{
-		//	_scrollThumb.style.left = event.clientX + "px";
-		}
-		
-		if (_isVertical == true)
-		{
-			_scroll = event.clientY;
-		}
-		else
-		{
-			_scroll = event.clientX;
-		}
-		
-		if (_onScroll != null)
-		{
-			var scrollEvent:UIEvent = new UIEvent();
-			scrollEvent.initUIEvent(UIEvent.SCROLL, false, false, 0.0);
-			_onScroll(scrollEvent);
+			scroll = event.clientX;
 		}
 	}
 	 
@@ -112,22 +148,44 @@ class ScrollBar extends HTMLElement
 	{
 		if (_isVertical == true)
 		{
-			_scrollThumb.style.top = event.clientY + "px";
+			scroll = event.clientY;
 		}
 		else
 		{
-			_scrollThumb.style.left = event.clientX + "px";
+			scroll = event.clientX;
 		}
+	}
+	
+	private function updateScroll():Void
+	{
+		if (_scroll > _maxScroll)
+		{
+			_scroll = maxScroll;
+		}
+		else if (_scroll < 0)
+		{
+			_scroll = 0;
+		}
+		
+		var progress:Float = scroll / maxScroll;
 		
 		if (_isVertical == true)
 		{
-			_scroll = event.clientY;
+			var thumbY:Int = Math.round(progress * (_coreStyle.computedStyle.height -
+			_upArrow.coreStyle.computedStyle.height - _downArrow.coreStyle.computedStyle.height - _scrollThumb.coreStyle.computedStyle.height)
+			+  _upArrow.coreStyle.computedStyle.height);
+			_scrollThumb.style.top = thumbY + "px";
 		}
 		else
 		{
-			_scroll = event.clientX;
+			_scrollThumb.style.left = scroll + "px";
 		}
 		
+		dispatchScrollEvent();
+	}
+	
+	private function dispatchScrollEvent():Void
+	{
 		if (_onScroll != null)
 		{
 			var scrollEvent:UIEvent = new UIEvent();
@@ -135,7 +193,6 @@ class ScrollBar extends HTMLElement
 			_onScroll(scrollEvent);
 		}
 	}
-	
 	
 	override private function isParentRendered():Bool
 	{
@@ -159,7 +216,9 @@ class ScrollBar extends HTMLElement
 	
 	private function set_maxScroll(value:Float):Float 
 	{
-		return _maxScroll = value;
+		_maxScroll = value;
+		//updateScroll();
+		return value;
 	}
 
 	private function get_scroll():Float
@@ -169,6 +228,8 @@ class ScrollBar extends HTMLElement
 	
 	private function set_scroll(value:Float):Float 
 	{
-		return _scroll = value;
+		_scroll = value;
+		updateScroll();
+		return value;
 	}
 }
