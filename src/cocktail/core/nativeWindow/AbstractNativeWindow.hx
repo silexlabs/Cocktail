3db338c66e87e924933d889bdc2d60ff651b79ed
yanhick <yannick.dominguez@gmail.com>
2012-05-22 18:45:09 +0200

    adding class to mutualise platform dependant code

---
+++ src/cocktail/core/nativeWindow/AbstractNativeWindow.hx
@@ -0,0 +1,144 @@
+/*
+	This file is part of Cocktail http://www.silexlabs.org/groups/labs/cocktail/
+	This project is © 2010-2011 Silex Labs and is released under the GPL License:
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	To read the license please visit http://www.gnu.org/copyleft/gpl.html
+*/
+package cocktail.core.nativeWindow;
+import cocktail.core.event.UIEvent;
+
+
+/**
+ * This class listens to native event dispatched
+ * by the window of the current platform, for 
+ * instance the Stage in flash
+ * 
+ * It then builds a cross-platform UIEvent
+ * from the dispatched native events,
+ * and call the callback corresponding to 
+ * the keyboard event type if provided
+ * 
+ * @author Yannick DOMINGUEZ
+ */
+class AbstractNativeWindow
+{
+	/**
+	 * The callback to call when
+	 * a native resize event is disptached
+	 */
+	private var _onResize:UIEvent->Void;
+	public var onResize(getOnResize, setOnResize):UIEvent->Void;
+	
+	/**
+	 * Height (in pixels) of the browser window viewport including,
+	 * if rendered, the horizontal scrollbar.
+	 */
+	public var innerHeight(get_innerHeight, never):Int;
+	
+	/**
+	 * Width (in pixels) of the browser window viewport including,
+	 * if rendered, the vertical scrollbar.
+	 */
+	public var innerWidth(get_innerWidth, never):Int;
+	
+	/**
+	 * class constructor
+	 */
+	public function new() 
+	{
+		//starts to listen to native input
+		setNativeListeners();
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// PUBLIC METHOD
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Open a new window using flash API
+	 */
+	public function open(url:String, name:String):Void
+	{
+		//abstract
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// EVENTS
+	// Private native event handler method
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Create a cross-platform resize event from
+	 * the native resize event, and call
+	 * the resize callback if provided
+	 * 
+	 * @param	event the native resize event
+	 */
+	private function onNativeResize(event:Dynamic):Void
+	{
+		if (onResize != null)
+		{
+			onResize(getUIEvent(event));
+		}
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// CALLBACKS SETTERS/GETTERS
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	private function setOnResize(value:UIEvent->Void):UIEvent->Void
+	{
+		return _onResize = value;
+	}
+	
+	private function getOnResize():UIEvent->Void
+	{
+		return _onResize;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// Private key utils methods
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Set listeners for platform specific events
+	 */
+	private function setNativeListeners():Void
+	{
+		//abstract
+	}
+	
+	/**
+	 * Remove listeners for platform specific events
+	 */
+	private function removeNativeListeners():Void
+	{
+		//abstract
+	}
+	
+	/**
+	 * Create and return a cross-platform ui event
+	 * from the dispatched native event
+	 * 
+	 * @param	event the native event
+	 */
+	private function getUIEvent(event:Dynamic):UIEvent
+	{
+		return null;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// GETTER/SETTER
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	private function get_innerHeight():Int
+	{
+		return -1;
+	}
+	
+	private function get_innerWidth():Int
+	{
+		return -1;
+	}
+}
---
+++ src/cocktail/port/flash_player/NativeWindow.hx
@@ -0,0 +1,105 @@
+/*
+	This file is part of Cocktail http://www.silexlabs.org/groups/labs/cocktail/
+	This project is © 2010-2011 Silex Labs and is released under the GPL License:
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	To read the license please visit http://www.gnu.org/copyleft/gpl.html
+*/
+package cocktail.port.flash_player;
+
+import cocktail.core.event.UIEvent;
+import cocktail.core.html.HTMLElement;
+import cocktail.core.NativeElement;
+import cocktail.core.event.KeyboardEvent;
+import cocktail.core.nativeWindow.AbstractNativeWindow;
+import flash.Lib;
+import flash.net.URLRequest;
+import haxe.Log;
+import cocktail.core.keyboard.AbstractKeyboard;
+
+/**
+ * This is the flash AVM2 implementation of the native window event manager.
+ * Listens to flash native resize event on the flash Stage.
+ * 
+ * @author Yannick DOMINGUEZ
+ */
+class NativeWindow extends AbstractNativeWindow
+{
+	/**
+	 * class constructor
+	 */
+	public function new() 
+	{
+		super();
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// OVERRIDEN PUBLIC METHOD
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Open a new window using flash API
+	 */
+	override public function open(url:String, name:String):Void
+	{
+		flash.Lib.getURL(new URLRequest(url), name);
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// Overriden private utils methods
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Set resize listeners on the stage
+	 */
+	override private function setNativeListeners():Void
+	{
+		Lib.current.stage.addEventListener(flash.events.Event.RESIZE, onNativeResize);
+	}
+	
+	/**
+	 * Remove resize listeners from the stage
+	 */
+	override private function removeNativeListeners():Void
+	{
+		Lib.current.stage.removeEventListener(flash.events.Event.RESIZE, onNativeResize);
+	}
+	
+	/**
+	 * Create and return a cross-platform resize event
+	 * from the flash event
+	 * 
+	 * @param	event the native event
+	 */
+	override private function getUIEvent(event:Dynamic):UIEvent
+	{
+		//cast the flash event
+		var typedEvent:flash.events.Event = cast(event);
+
+		var resizeEvent:UIEvent = new UIEvent();
+		resizeEvent.initUIEvent(UIEvent.RESIZE, false, false, null, 0.0);
+		
+		return resizeEvent;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// Overriden GETTER/SETTER
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Return the flash Stage height
+	 */
+	override private function get_innerHeight():Int
+	{
+		return Lib.current.stage.stageHeight;
+	}
+	
+	/**
+	 * Return the flash Stage width
+	 */
+	override private function get_innerWidth():Int
+	{
+		return Lib.current.stage.stageWidth;
+	}
+	
+}
