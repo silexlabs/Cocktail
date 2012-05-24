99494582d1a4b5a61a83d97179ce06691b797510
codam <raphael.harmel@gmail.com>
2012-05-24 16:04:55 +0200

    W3C unit tests updates & rename

--- layout_test/visual_formatting_model_details/definition_of_containing_block/containing-block-017/js.js
+++ layout_test/visual_formatting_model_details/definition_of_containing_block/containing-block-017/js.js
@@ -47,12 +47,18 @@ Std.prototype = {
 	__class__: Std
 }
 var Test = $hxClasses["Test"] = function() {
-	var test = "<div><p>Test passes if the blue box is in the lower-right corner of the black box.</p>";
-	test += "<div style=\"position:relative; width:0; padding:1in;\">";
-	test += "<span style=\"display:block;\">";
-	test += "<span style=\"background-color:blue; height:1in; width:1in; position:absolute; display:block;\"></span>";
+	var test = "";
+	test += "<div>";
+	test += "<p>Test passes if there is no red visible on the page.</p>";
+	test += "<div style=\"direction: ltr; margin-bottom: 20px; padding: 100px; width: 450px;\">";
+	test += "<span id=\"test\" style=\"padding: 50px; position: relative;\">";
+	test += "<span id=\"first-box\" style=\"color: silver;\"><span id=\"tl-control\" style=\"margin-left: -50px; margin-right: 20px; padding: 20px 15px;\"></span>Filler Text Filler Text Filler Text Filler Text</span>";
+	test += "<span style=\"height: 30px; position: absolute; width: 30px; background: green; bottom: 0; right: 0;\">BR</span>";
+	test += "<span style=\"height: 30px; position: absolute; width: 30px; background: green; left: 0; top: 0;\">TL</span>";
+	test += "<span id=\"last-box\" style=\"color: silver;\">Filler Text Filler Text Filler Text Filler Text<span id=\"br-control\" style=\"margin-left: 20px; margin-right: -50px; padding: 20px 15px;\"></span></span>";
 	test += "</span>";
-	test += "</div></div>";
+	test += "</div>";
+	test += "</div>";
 	js.Lib.document.body.innerHTML = test;
 };
 Test.__name__ = ["Test"];
