1db464dde563da8d3e6918070e713f10c771fd00
Yannick DOMINGUEZ <yannick.dominguez@gmail.com>
2012-05-31 11:05:47 +0200

    working on flash video

--- src/cocktail/port/platform/nativeMedia/NativeMedia.hx
+++ src/cocktail/port/platform/nativeMedia/NativeMedia.hx
@@ -1,28 +1,79 @@
+/*
+	This file is part of Cocktail http://www.silexlabs.org/groups/labs/cocktail/
+	This project is © 2010-2011 Silex Labs and is released under the GPL License:
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	To read the license please visit http://www.gnu.org/copyleft/gpl.html
+*/
 package cocktail.port.platform.nativeMedia;
+
 import cocktail.core.event.Event;
 import cocktail.core.NativeElement;
 
 /**
- * ...
+ * This is a base class proxying access to 
+ * platform specific media API for video and
+ * audio
+ * 
  * @author Yannick DOMINGUEZ
  */
-
 class NativeMedia
 {
-	
+	/**
+	 * get the duration in second of the
+	 * native media, available as soon as 
+	 * the meta data are loaded
+	 */
 	public var duration(get_duration, never):Float;
 	
-	private var volume(get_volume, set_volume):Float;
+	/**
+	 * set the volume of the native media
+	 */
+	public var volume(never, set_volume):Float;
 	
+	/**
+	 * set the source of th native media, starting loading
+	 * it
+	 */
 	public var src(never, set_src):String;
+	
+	/**
+	 * return the instrinsic width of
+	 * the native media
+	 */
+	public var width(get_width, never):Float;
+	
+	/**
+	 * return the instrinsic height of
+	 * the native media
+	 */
+	public var height(get_height, never):Float;
+	
+	/**
+	 * Return the current playhead time, in
+	 * seconds
+	 */
+	public var currentTime(get_currentTime, never):Float;
 
+	/**
+	 * A reference to the nativeElement of the native media, used to attach
+	 * it to the rendering tree
+	 */
+	public var nativeElement(get_nativeElement, never):NativeElement;
+	
+	/**
+	 * Listen to the native event dispatched when the metadata
+	 * of the native media are loaded
+	 */
 	private var _onLoadedMetaData:Event->Void;
 	public var onLoadedMetaData(get_onLoadedMetaData, set_onLoadedMetaData):Event->Void;
 	
+	/**
+	 * Listen to tick native event dispatched by the native media
+	 * while playing
+	 */
 	private var _onTimeUpdate:Event->Void;
 	public var onTimeUpdate(get_onTimeUpdate, set_onTimeUpdate):Event->Void;
-	
-	public var nativeElement(get_nativeElement, never):NativeElement;
 	
 	/**
 	 * class constructor
@@ -36,19 +87,31 @@ class NativeMedia
 	// PUBLIC METHOD
 	//////////////////////////////////////////////////////////////////////////////////////////
 	
+	/**
+	 * play/resume the the native media
+	 */
 	public function play():Void
 	{
-		
+		//abstract
 	}
 	
+	/**
+	 * pause the native media
+	 */
 	public function pause():Void
 	{
-		
+		//abstract
 	}
 	
+	/**
+	 * Seek to the specified time in the 
+	 * native media
+	 * @param	time the time to seek to,
+	 * in seconds
+	 */
 	public function seek(time:Float):Void
 	{
-		
+		//abstract
 	}
 	
 	//////////////////////////////////////////////////////////////////////////////////////////
@@ -56,7 +119,12 @@ class NativeMedia
 	// Private native video event handler method
 	//////////////////////////////////////////////////////////////////////////////////////////
 	
-	private function onNativeLoadedMetaData(event:Dynamic):Void
+	/**
+	 * When a native event is dispatching signaling that the
+	 * meata data of the native media are loaded, dispatch
+	 * a cross-platform metadata loaded event
+	 */
+	private function onNativeLoadedMetaData():Void
 	{
 		if (_onLoadedMetaData != null)
 		{
@@ -66,6 +134,10 @@ class NativeMedia
 		}
 	}
 	
+	/**
+	 * When a native tick event is dispatched by the native
+	 * media, dispatch a cross-platform tick event
+	 */
 	private function onNativeTimeUpdate(event:Dynamic):Void
 	{
 		if (_onTimeUpdate != null)
@@ -103,6 +175,21 @@ class NativeMedia
 	// SETTERS/GETTERS
 	//////////////////////////////////////////////////////////////////////////////////////////
 	
+	private function get_currentTime():Float
+	{
+		return 0;
+	}
+	
+	private function get_height():Float
+	{
+		return 0;
+	}
+	
+	private function get_width():Float
+	{
+		return 0;
+	}
+	
 	private function get_nativeElement():NativeElement
 	{
 		return null;
@@ -117,12 +204,6 @@ class NativeMedia
 	{
 		return value;
 	}
-	
-	private function get_volume():Float
-	{
-		return 0;
-	}
-	
 	private function set_volume(value:Float):Float
 	{
 		return value;
