80be41b8973fbeb371effc71c7097e32ccc9ef85
yanhick <yannick.dominguez@gmail.com>
2012-07-19 10:49:03 +0200

    removed duplicated code

--- src/cocktail/core/renderer/EmbeddedBoxRenderer.hx
+++ src/cocktail/core/renderer/EmbeddedBoxRenderer.hx
@@ -8,6 +8,7 @@
 package cocktail.core.renderer;
 import cocktail.core.html.HTMLElement;
 import cocktail.port.DrawingManager;
+import cocktail.core.geom.GeomData;
 
 /**
  * Base class for embedded element
@@ -55,5 +56,82 @@ class EmbeddedBoxRenderer extends BoxRenderer
 	private function renderEmbeddedAsset(graphicContext:DrawingManager)
 	{
 		//abstract
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// PRIVATE UTILS METHODS
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Utils method returning the right rectangle so that
+	 * a picture or video can take the maximum available width
+	 * and height while preserving their aspect ratio and also be 
+	 * centered in the available space
+	 * 
+	 * @param	availableWidth the maximum width available for the picture or video
+	 * @param	availableHeight the maximum height available for the picture or video
+	 * @param	assetWidth the intrinsic width of the video or picture
+	 * @param	assetHeight the intrinsic height of the video or picture
+	 * @return	the bounds of the asset
+	 */
+	private function getAssetBounds(availableWidth:Float, availableHeight:Float, assetWidth:Float, assetHeight:Float):RectangleData
+	{
+		//those will hold the actual value used for the video or poster 
+		//dimensions, with the kept aspect ratio
+		var width:Float;
+		var height:Float;
+
+		if (availableWidth > availableHeight)
+		{
+			//get the ratio between the intrinsic asset width and the width it must be displayed at
+			var ratio:Float = assetHeight / availableHeight;
+			
+			//check that the asset isn't wider than the available width
+			if ((assetWidth / ratio) < availableWidth)
+			{
+				//the asset width use the computed width while the height apply the ratio
+				//to the asset height, so that the ratio is kept while displaying the asset
+				//as big as possible
+				width =  assetWidth / ratio ;
+				height = availableHeight;
+			}
+			//else reduce the height instead of the width
+			else
+			{
+				ratio = assetWidth / availableWidth;
+				
+				width = availableWidth;
+				height = assetHeight / ratio;
+			}
+		}
+		//same as above but inverted
+		else
+		{
+			var ratio:Float = assetWidth / availableWidth;
+			
+			if ((assetHeight / ratio) < availableHeight)
+			{
+				height = assetHeight / ratio;
+				width = availableWidth;
+			}
+			else
+			{
+				ratio = assetHeight / availableHeight;
+				width =  assetWidth / ratio ;
+				height = availableHeight;
+			}
+		}
+		
+		//the asset must be centered in the ElementRenderer, so deduce the offsets
+		//to apply to the x and y direction
+		var xOffset:Float = (availableWidth - width) / 2;
+		var yOffset:Float = (availableHeight - height) / 2;
+		
+		return {
+			width:width,
+			height:height,
+			x:xOffset,
+			y:yOffset
+		}
 	}
 }
