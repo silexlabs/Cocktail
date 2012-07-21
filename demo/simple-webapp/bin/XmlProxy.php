23563b16729eb579f7d24e9ef44a4234dde44b9e
yanhick <yannick.dominguez@gmail.com>
2012-07-21 12:10:12 +0200

    deleted obsolete demos

--- demo/simple-webapp/bin/XmlProxy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header ("Content-Type:text/xml");
-header('Access-Control-Allow-Origin: *');
-
-echo file_get_contents($_REQUEST['url']);
-
-?>
--- demo/simple-webapp/build/native_apps/WebApp_air_android.bat
+++
@@ -1,7 +0,0 @@
-ECHO OFF
-ECHO.
-ECHO generating android version via Air...
-CALL adt -package -target apk -storetype pkcs12 -keystore sampleCert.pfx -storepass password ../../bin/native_apps/WebApp_android_Air.apk WebApp_air.xml icons -C ../../bin WebApp.swf images
-ECHO.
-ECHO generation complete
-ECHO.
