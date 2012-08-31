c844998ec20bbde1a3396151a5a0dd25c36ab5f9
codam <raphael.harmel@gmail.com>
2012-08-31 19:14:06 +0200

    Updated license to MIT

--- samples/GettingStarted/src/Main.hx
+++ samples/GettingStarted/src/Main.hx
@@ -1,3 +1,11 @@
+/*
+ * Cocktail, HTML rendering engine
+ * http://haxe.org/com/libs/cocktail
+ *
+ * Copyright (c) Silex Labs
+ * Cocktail is available under the MIT license
+ * http://www.silexlabs.org/labs/cocktail-licensing/
+*/
 import haxe.Http;
 import js.Lib;
 import js.Dom;
--- test/layout_tests/font/font-size-094/bin/lib/cocktail/core/style/PositionOffset.enum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class cocktail_core_style_PositionOffset extends Enum {
-	public static $cssAuto;
-	public static function length($value) { return new cocktail_core_style_PositionOffset("length", 0, array($value)); }
-	public static function percent($value) { return new cocktail_core_style_PositionOffset("percent", 1, array($value)); }
-	public static $__constructors = array(2 => 'cssAuto', 0 => 'length', 1 => 'percent');
-	}
-cocktail_core_style_PositionOffset::$cssAuto = new cocktail_core_style_PositionOffset("cssAuto", 2);
