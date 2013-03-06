48c3a7f631be87ea243642fa034131916638d3c3
yanhick <yannick.dominguez@gmail.com>
2013-03-06 11:49:25 +0100

    removed layout test, now replaced by css tester

--- test/layout_tests/font/font-size-094/bin/lib/cocktail/core/style/ConstrainedDimension.enum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class cocktail_core_style_ConstrainedDimension extends Enum {
-	public static function length($value) { return new cocktail_core_style_ConstrainedDimension("length", 0, array($value)); }
-	public static $none;
-	public static function percent($value) { return new cocktail_core_style_ConstrainedDimension("percent", 1, array($value)); }
-	public static $__constructors = array(0 => 'length', 2 => 'none', 1 => 'percent');
-	}
-cocktail_core_style_ConstrainedDimension::$none = new cocktail_core_style_ConstrainedDimension("none", 2);
--- test/layout_tests/font/font-size-094/bin/lib/cocktail/core/style/computer/boxComputers/EmbeddedInlineBlockBoxStylesComputer.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class cocktail_core_style_computer_boxComputers_EmbeddedInlineBlockBoxStylesComputer extends cocktail_core_style_computer_boxComputers_EmbeddedInlineBoxStylesComputer {
-	public function __construct() { if(!php_Boot::$skip_constructor) {
-		parent::__construct();
-	}}
-	function __toString() { return 'cocktail.core.style.computer.boxComputers.EmbeddedInlineBlockBoxStylesComputer'; }
-}
