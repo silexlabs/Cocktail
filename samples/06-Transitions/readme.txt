30e76cf532cc926d59d0c78e9da553ea9d24131b
yanhick <yannick.dominguez@gmail.com>
2013-03-22 11:35:50 +0100

    added transition sample

---
+++ samples/06-Transitions/readme.txt
@@ -0,0 +1,18 @@
+/*
+ * Cocktail, HTML rendering engine
+ * http://haxe.org/com/libs/cocktail
+ *
+ * Copyright (c) Silex Labs
+ * Cocktail is available under the MIT license
+ * http://www.silexlabs.org/labs/cocktail-licensing/
+*/
+
+======================
+== Cocktail Samples ==
+======================
+
+Transitions
+--------------
+
+Description: animate divs using CSS transitions
+Tested targets: JavaScript, Flash
---
+++ samples/06-Transitions/src/Main.hx
@@ -0,0 +1,73 @@
+/*
+ * Cocktail, HTML rendering engine
+ * http://haxe.org/com/libs/cocktail
+ *
+ * Copyright (c) Silex Labs
+ * Cocktail is available under the MIT license
+ * http://www.silexlabs.org/labs/cocktail-licensing/
+*/
+import haxe.Timer;
+import js.Lib;
+import js.Dom;
+
+/**
+ * Add divs to the document and 
+ * moves them randomly using CSS
+ * transitions
+ */
+class Main
+{
+	public function new()
+	{
+		addTransitionBox();
+	}
+	
+	/**
+	 * Moves a box randomly
+	 */
+	function transition(transitionBox:HtmlDom)
+	{
+		var container = Lib.document.getElementById("container");
+		
+		var transWidth = transitionBox.clientWidth;
+		var transHeight = transitionBox.clientHeight;
+		var containerWidth = container.clientWidth;
+		var containerHeight = container.clientHeight;
+		
+		transitionBox.style.left = Std.string(Math.round(Math.random() * (containerWidth - transWidth))) + "px";
+		transitionBox.style.top = Std.string(Math.round(Math.random() * (containerHeight - transHeight))) + "px";
+	}
+	
+	/**
+	 * create a new box
+	 */
+	function addTransitionBox()
+	{
+		var transitionBox = Lib.document.createElement("div");
+		transitionBox.className = "trans";
+		
+		var randColor = function() {return Math.floor(Math.random() * 255); };
+		
+		transitionBox.style.backgroundColor = "rgb(" + randColor() + "," + randColor() + "," + randColor() + ")";
+		
+		transitionBox.addEventListener("transitionend",
+		function(e) {
+			transition(transitionBox);
+		},
+		false);
+		
+		transitionBox.addEventListener("webkitTransitionEnd",
+		function(e) {
+			transition(transitionBox);
+		},
+		false);
+		
+		transitionBox.onclick = function(e) {
+			addTransitionBox();
+		}
+		
+		Lib.document.getElementById("container").appendChild(transitionBox);
+		
+		Timer.delay(function() {transition(transitionBox);}, 1000);
+	}
+}
