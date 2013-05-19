8c49b67d3a31cfa726fb16a6514ecb6f32c2ffb8
Yannick Dominguez <yannick.dominguez@gmail.com>
2013-05-19 18:17:08 +0200

    added window type

---
+++ cocktail/html/DOMWindow.hx
@@ -0,0 +1,10 @@
+/*
+ * Cocktail, HTML rendering engine
+ * http://haxe.org/com/libs/cocktail
+ *
+ * Copyright (c) Silex Labs
+ * Cocktail is available under the MIT license
+ * http://www.silexlabs.org/labs/cocktail-licensing/
+*/
+
+typedef DOMWindow = cocktail.core.window.Window;
