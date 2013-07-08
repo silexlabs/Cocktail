1223941da8cecbbf2b6542bf7306b35583ae8757
Yannick Dominguez <yannick.dominguez@gmail.com>
2013-07-08 21:05:12 +0200

    added sample for embedded font

---
+++ samples/10-embedded_font/src/Main.hx
@@ -0,0 +1,24 @@
+
+/*
+ * Cocktail, HTML rendering engine
+ * http://haxe.org/com/libs/cocktail
+ *
+ * Copyright (c) Silex Labs
+ * Cocktail is available under the MIT license
+ * http://www.silexlabs.org/labs/cocktail-licensing/
+*/
+
+/**
+ * Showcases how to use embedded font when building with
+ * the nme tool
+ */
+class Main
+{
+	static function main()
+	{
+		#if !js
+		//init cocktail, and starts the load of the "index.html" file
+		cocktail.api.Cocktail.boot();
+		#end
+	}
+}
