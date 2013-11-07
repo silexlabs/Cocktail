3a1f7c6d53eae1b2572e1b9342aa00d551c29830
Yannick <yannick.dominguez@gmail.com>
2013-11-07 23:54:15 +0100

    improved video sample

--- samples/11-video/src/Main.hx
+++ samples/11-video/src/Main.hx
@@ -8,6 +8,8 @@
  * http://www.silexlabs.org/labs/cocktail-licensing/
 */
 
+import js.Browser;
+
 /**
  * Showcases how to display a video in flash
  */
@@ -19,5 +21,28 @@ class Main
 		//init cocktail, and starts the load of the "index.html" file
 		cocktail.api.Cocktail.boot();
 		#end
+
+		Browser.window.onload = function(e) new Main();
 	}
+
+    public function new()
+    {
+        var video:js.html.VideoElement = cast Browser.document.getElementsByTagName("video")[0];
+        var playBtn = Browser.document.querySelector("input[type=button][value=play]");
+        var pauseBtn = Browser.document.querySelector("input[type=button][value=pause]");
+        var time = Browser.document.getElementsByClassName("time")[0];
+
+        playBtn.onclick = function(e) {
+            video.play();
+        }
+
+        pauseBtn.onclick = function(e) {
+            video.pause();
+        }
+
+        video.ontimeupdate = function(e) {
+            time.style.width = Std.string((video.currentTime / video.duration) * 100) + "px";
+        }
+
+    }
 }
