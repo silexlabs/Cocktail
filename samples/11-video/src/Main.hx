2d6dea357f68a3662868707855f68153df5c114c
Yannick <yannick.dominguez@gmail.com>
2013-11-12 21:45:47 +0100

    improving video sample

--- samples/11-video/src/Main.hx
+++ samples/11-video/src/Main.hx
@@ -30,11 +30,16 @@ class Main
     public function new()
     {
         var video:js.html.VideoElement = cast Browser.document.getElementsByTagName("video")[0];
+
         var playBtn:InputElement = cast Browser.document.querySelector("input[type=button][value=play]");
+
         var time:HtmlElement = cast Browser.document.getElementsByClassName("time")[0];
         var track:HtmlElement = cast Browser.document.getElementsByClassName("track")[0];
+        var player:HtmlElement = cast Browser.document.getElementsByClassName("player")[0];
+        var muteBtn:InputElement = cast Browser.document.getElementsByClassName("mute")[0];
+        var fullscreenBtn:InputElement = cast Browser.document.getElementsByClassName("full")[0];
 
-        //play pause
+        //play/pause
         playBtn.onclick = function(e) {
             if (playBtn.value == "play") {
                 playBtn.value = "pause";
@@ -43,6 +48,18 @@ class Main
             else {
                 playBtn.value = "play";
                 video.pause();
+            }
+        }
+
+        //mute/unmute
+        muteBtn.onclick = function(e) {
+            if (muteBtn.value == "mute") {
+                muteBtn.value = "unmute";
+                video.muted = true;
+            }
+            else {
+                muteBtn.value = "mute";
+                video.muted = false;
             }
         }
 
@@ -55,5 +72,20 @@ class Main
         track.onclick = function(e) {
             video.currentTime = ((e.clientX - track.offsetLeft) / track.clientWidth) * video.duration;
         }
+
+        //toggle fullscreen
+        //note : for fullscreen to work in flash, the swf need to embedded with the
+        //"allowFullScreen" param set to true
+        fullscreenBtn.onclick = function(e) {
+            if (fullscreenBtn.value == "full") {
+                fullscreenBtn.value = "small";
+                player.requestFullScreen();
+            }
+            else {
+                fullscreenBtn.value = "full";
+                Browser.document.exitFullscreen();
+            }
+        }
+
     }
 }
