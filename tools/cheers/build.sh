51a4ed6e95bbec91ba575c679a9d2b34ddd1d687
Yannick Dominguez <yannick.dominguez@gmail.com>
2015-11-12 13:51:31 -0800

    make it work with both js and flash

--- tools/cheers/build.sh
+++ tools/cheers/build.sh
@@ -1,5 +1,6 @@
-haxe build_cheers.hxml
+haxe build_cheers.hxml -D build_for_flash
 openfl build html5 -debug
 cp -r ./bin/html5/bin/res ./bin
 cp ./bin/html5/bin/CocktailDemo.js ./bin
 cp ./bin/html5/bin/CocktailDemo.js.map ./bin
+openfl build flash -debug
