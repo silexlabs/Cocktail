fe22433d1aca1c2bd85991070d062dbcc90601d5
Yannick Dominguez <yannick.dominguez@gmail.com>
2015-11-13 11:30:06 -0800

    update embedded font sample

--- samples/10-embedded_font/src/Main.hx
+++ samples/10-embedded_font/src/Main.hx
@@ -1,4 +1,4 @@
-
+package src;
 /*
  * Cocktail, HTML rendering engine
  * http://haxe.org/com/libs/cocktail
@@ -16,9 +16,7 @@ class Main
 {
 	static function main()
 	{
-		#if !js
 		//init cocktail, and starts the load of the "index.html" file
-		cocktail.api.Cocktail.boot();
-		#end
+		cocktail.api.Cocktail.boot("res/index.html");
 	}
 }
